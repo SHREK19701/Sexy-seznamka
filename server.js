252b16ac4923557f7d3870f886b529e99bb8b5ef
SHREK19701 <jardaolaholah@gmail.com>
2024-11-14 08:54:27 +0100

    Add files via upload
    Signed-off-by: SHREK19701 <jardaolaholah@gmail.com>

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const http = require('http');
-const path = require('path'); // Zajistíme, že path je nactený modul
+const path = require('path');
+const { Pool } = require('pg');
+const socketIo = require('socket.io'); // Přidáme socket.io
+const session = require('express-session'); // Přidáme express-session pro správu session
 
 const app = express();
 const server = http.createServer(app);
@@ -13,20 +16,32 @@ const pool = new Pool({
     host: 'localhost',
     database: 'db_url',
     password: 'Charalamba11@',
-    port: 5432, // nebo váš vlastní port
+    port: 5432,
 });
 
-// Overení pripojení k databázi
+// Ověření připojení k databázi
 pool.connect()
     .then(client => {
-        console.log('Pripojeno k databázi');
-        client.release(); // Uvolníme klienta zpet do poolu
+        console.log('Připojeno k databázi');
+        client.release();
     })
     .catch(err => {
-        console.error('Chyba pri pripojování k databázi:', err.stack);
+        console.error('Chyba při připojování k databázi:', err.stack);
     });
 
-// Nastavení statických souboru
+// Middleware pro session
+app.use(session({
+    secret: 'tajnyklic', // nahraďte vlastním tajným klíčem
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // secure: true použijte jen při HTTPS
+}));
+
+// Middleware pro zpracování JSON a URL encoded dat
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Nastavení statických souborů
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Hlavní stránka - index.html
@@ -39,7 +54,7 @@ app.get('/registrace', (req, res) => {
     res.sendFile(path.join(__dirname, 'registrace.html'));
 });
 
-// Prihlášení
+// Přihlášení
 app.get('/prihlaseni', (req, res) => {
     res.sendFile(path.join(__dirname, 'prihlaseni.html'));
 });
@@ -59,17 +74,12 @@ app.get('/koupit-mince', (req, res) => {
     res.sendFile(path.join(__dirname, 'koupit mince.html'));
 });
 
-// Komentáre
+// Komentáře
 app.get('/komentare', (req, res) => {
     res.sendFile(path.join(__dirname, 'komentare.html'));
 });
 
-// Spuštení serveru
-server.listen(port, () => {
-    console.log(`Server beží na http://localhost:${port}`);
-});
-
-// Prihlašovací endpoint
+// Přihlašovací endpoint
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
@@ -92,24 +102,29 @@ function isModerator(req, res, next) {
 
 // Route pro moderátora
 app.get('/moderator', isModerator, (req, res) => {
-    res.render('moderator');
+    res.sendFile(path.join(__dirname, 'moderator.html')); // render změněno na sendFile
 });
 
-// Služba pro obsluhu klientu a moderátora
+// Služba pro obsluhu klientů a moderátora
 let clients = [];
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    console.log('Uživatel připojen');
     clients.push(socket);
 
     socket.on('clientMessage', (message) => {
-        console.log('Message from client:', message);
+        console.log('Zpráva od klienta:', message);
         io.emit('newMessage', message);
     });
 
     socket.on('disconnect', () => {
-        console.log('A user disconnected');
+        console.log('Uživatel odpojen');
         clients = clients.filter(client => client !== socket);
     });
+});
+
+// Spuštění serveru
+server.listen(port, () => {
+    console.log(`Server běží na http://localhost:${port}`);
 });
 
