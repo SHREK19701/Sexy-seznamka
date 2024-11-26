6793130343ad11df298902a459d7dfed7fed2917
SHREK19701 <jardaolaholah@gmail.com>
2024-11-26 10:19:05 +0100

    Add files via upload
    Signed-off-by: SHREK19701 <jardaolaholah@gmail.com>

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Nastavení statických souborů
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'index.html')));
 
 // Různé stránky
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
